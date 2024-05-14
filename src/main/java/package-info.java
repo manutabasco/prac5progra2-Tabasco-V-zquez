8d798a7a8bfe9fd895b354aa98739af388fb2da3
Manuel Tabasco <manuel.tabasco@usp.ceu.es>
2024-05-14 11:34:24 +0200

    entrega final p5 sin readme

---
+++ src/main/java/package-info.java
@@ -0,0 +1,9 @@
+package java;
+
+/**
+ * Proporciona clases para la representación y manipulación de grafos
+ *
+ * Este paquete contiene la clase Graph.java que permite crear y gestionar un grafo dirigido
+ * con operaciones básicas como añadir vértices, añadir arcos, obtener adyacentes, verificar la
+ * existencia de vértices y encontrar caminos más cortos entre dos vértices.
+ */
---
+++ src/test/java/package-info.java
@@ -0,0 +1,8 @@
+package java;
+
+/**
+ * Proporciona clases para la representación y manipulación de grafos,
+ * así como para la realización de pruebas unitarias de estas operaciones.
+ * 
+ * Contiene la clase Graph.test la cual contiene pruebas unitarias para la clase Graph
+ */
